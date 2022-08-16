a4cc5eb754ad3247a696a6bddcb74e1f50db2633
CH Dharma Rao <chdharmarao21@gmail.com>
2022-08-17 03:06:35 +0530

    Add Articles, Pictures and Category Tabs with pagination

--- app/src/main/java/com/example/wikiapp/MainActivity.kt
+++ app/src/main/java/com/example/wikiapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.wikiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.example.wikiapp.adapters.ViewPageAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    var tabLayout: TabLayout? = null
+    var viewPager: ViewPager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        viewPager = findViewById<ViewPager>(R.id.viewpager)
+        tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = ViewPageAdapter( supportFragmentManager, tabLayout!!.tabCount)
+        viewPager!!.adapter = adapter
+
+        viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager!!.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
     }
 }
---
+++ app/src/main/java/com/example/wikiapp/adapters/ViewPageAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.wikiapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.wikiapp.view.ArticlesFragment
+import com.example.wikiapp.view.CategoriesFragment
+import com.example.wikiapp.view.PicturesFragment
+
+class ViewPageAdapter(fm: FragmentManager, private val numofTabs: Int) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return numofTabs
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when (position) {
+            0 -> {
+                return ArticlesFragment()
+            }
+            1 -> {
+                return PicturesFragment()
+            }
+            2 -> {
+                return CategoriesFragment()
+            }
+            else -> return ArticlesFragment()
+        }      }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        var title: String? = null
+        if (position == 0) title = "Articles" else if (position == 1) title =
+            "Pictures" else if (position == 2) title = "Categories"
+        return title
+    }
+
+
+}
